ffae7bb03c4d77c78318d166732d4af54a67b1ca
Christopher Arndt <chris@chrisarndt.de>
2016-10-12 19:32:57 +0200

    Change license to GPL2 comply with that of included jacklib module; change version number to reflect beta stage; complete meta-data classifiers
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 
 from setuptools import setup
 
+
 setup(
     name='jack-matchmaker',
-    version="1.0",
+    version="0.1.0",
     description="Auto-connect new JACK ports.",
-    #long_description='',
+    long_description=open('README.rst').read(),
     author="Christopher Arndt",
     author_email="info@chrisarndt.de",
     url="https://github.com/SpotlightKid/jack-matchmaker",
-    license="MIT License",
+    license="GPL2",
     packages=["jackmatchmaker"],
     entry_points={
         "console_scripts": [
@@ -19,16 +20,20 @@ setup(
         ]
     },
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        "Topic :: Multimedia :: Sound/Audio",
-        "Topic :: Multimedia :: Sound/Audio :: MIDI",
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: MIDI',
+        'Topic :: Utilities',
     ]
 )
