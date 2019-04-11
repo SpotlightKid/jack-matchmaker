d71e76e7de44f7413c85ed6601d4b352b81cf30c
Christopher Arndt <chris@chrisarndt.de>
2019-04-11 23:40:40 +0200

    Simplify 'list_port_info' script
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- jacklib-examples/list_port_info.py
+++ jacklib-examples/list_port_info.py
@@ -5,7 +5,7 @@ import sys
 from itertools import chain
 
 from jackmatchmaker import jacklib
-from jackmatchmaker.jacklib_helpers import get_jack_status_error_string
+from jackmatchmaker.jacklib_helpers import c_char_p_p_to_list, get_jack_status_error_string
 
 
 status = jacklib.jack_status_t()
@@ -15,42 +15,13 @@ if status:
     err = get_jack_status_error_string(status)
     sys.exit("Error connecting to JACK server: %s" % err)
 
-print("Output ports:\n")
-for portname in jacklib.get_ports(client, '', '', jacklib.JackPortIsOutput):
-    if portname is None:
-        break
-
-    portname = portname.decode('utf-8')
+for portname in c_char_p_p_to_list(jacklib.get_ports(client)):
     port = jacklib.port_by_name(client, portname)
     uuid = jacklib.port_uuid(port)
 
-    print("Port %s\nUUID: %s" % (portname, uuid))
+    print("Port name: %s\nUUID: %s" % (portname, uuid))
     num_aliases, *aliases = jacklib.port_get_aliases(port)
-    print("Aliases: %s" % ", ".join(aliases[:num_aliases]))
-
-    props = jacklib.get_port_properties(client, portname)
-    if props:
-        print("Properties:")
-        for prop in props:
-            print(" * {}: {} (type: {})".format(prop.key, prop.value, prop.type))
-
-    pretty_name = jacklib.get_port_pretty_name(client, portname)
-    if pretty_name:
-        print("Pretty-name: {}".format(pretty_name))
-
-    print('')
-
-print("\nInput ports:\n")
-for portname in jacklib.get_ports(client, '', '', jacklib.JackPortIsInput):
-    if portname is None:
-        break
-
-    portname = portname.decode('utf-8')
-    port = jacklib.port_by_name(client, portname)
-    uuid = jacklib.port_uuid(port)
-
-    print("Port %s\nUUID: %s" % (portname, uuid))
-    print("Aliases: %s" % ", ".join(aliases[:num_aliases]))
+    print("Aliases: %s" % " ".join(aliases[:num_aliases]))
 
     props = jacklib.get_port_properties(client, portname)
     if props:
